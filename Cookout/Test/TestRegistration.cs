54d0178f2fa92a7def78e374357bd09db9a2b654
Will Thompson <n9701192@qut.edu.au>
2018-11-02 21:06:54 +1000

    Final Final Commit

---
+++ Cookout/Test/TestRegistration.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using NUnit.Framework;
+using Xamarin.UITest;
+
+namespace tryyyy.UITests
+{
+    [TestFixture(Platform.Android)]
+    public class TestRegistration
+    {
+        IApp app;
+        Platform platform;
+        Stopwatch stopwatch;
+
+
+
+        public TestRegistration(Platform platform)
+        {
+            this.platform = Platform.Android;
+        }
+
+        [SetUp]
+        public void BeforeEachTest()
+        {
+            app = AppInitializer.StartApp(platform);
+            try{
+                app.Tap("redirectregistration");
+            }
+            catch(Exception)
+            {
+                Debug.WriteLine("it doesn't exist in the other page");
+            }
+            stopwatch = new Stopwatch();
+            //app.Screenshot("On " + this.GetType().Name);
+
+        }
+
+        /*   */
+        //==================================test whether text field can be empty
+        [Test]
+        public void RegistrationEmptyTest1()
+        {
+            //EntryInputText(string first, string last, string mobile, string email, string password, string confirm_password)
+            EntryInputText("afawfawf", "", "", "", "", "");
+            WaitAndTest(5000, "You can't leave any field empty.");
+        }
+        [Test]
+        public void RegistrationEmptyTest2()
+        {
+            //EntryInputText(string first, string last, string mobile, string email, string password, string confirm_password)
+            EntryInputText("", "faefwafw", "", "", "", "");
+            WaitAndTest(5000, "You can't leave any field empty.");
+
+        }
+        [Test]
+        public void RegistrationEmptyTest3()
+        {
+            //EntryInputText(string first, string last, string mobile, string email, string password, string confirm_password)
+            EntryInputText("", "", "fqwfqwfqw", "", "", "");
+            WaitAndTest(5000, "You can't leave any field empty.");
+        }
+        [Test]
+        public void RegistrationEmptyTest4()
+        {
+            //EntryInputText(string first, string last, string mobile, string email, string password, string confirm_password)
+            EntryInputText("", "", "", "qwfqwfqwf", "", "");
+            WaitAndTest(5000, "You can't leave any field empty.");
+        }
+        [Test]
+        public void RegistrationEmptyTest5()
+        {
+            //EntryInputText(string first, string last, string mobile, string email, string password, string confirm_password)
+            EntryInputText("", "", "", "", "qwfqwfqwf", "");
+            WaitAndTest(5000, "You can't leave any field empty.");
+        }
+        [Test]
+        public void RegistrationEmptyTest6()
+        {
+            //EntryInputText(string first, string last, string mobile, string email, string password, string confirm_password)
+            EntryInputText("", "", "", "", "", "qefqfqwfqw");
+            WaitAndTest(5000, "You can't leave any field empty.");
+        }
+        [Test]
+        public void RegistrationEmptyTest7()
+        {
+            //EntryInputText(string first, string last, string mobile, string email, string password, string confirm_password)
+            EntryInputText("", "", "", "", "", "");
+            WaitAndTest(5000, "You can't leave any field empty.");
+        }
+        //==================================test whether text field can be empty
+
+        //check when email already exists in db. email is unique column in db.
+        [Test]
+        public void RegistrationEmailAlreadyExists()
+        {
+            //EntryInputText(string first, string last, string mobile, string email, string password, string confirm_password)
+            EntryInputText("firstname", "lastname", "0401420594", "yyoon91@gmail.com", "bbbbbb", "bbbbbb");
+            WaitAndTest(10000, "The email already exists. Please use a different email.");
+        }
+
+        //password doesn't match
+        [Test]
+        public void RegistrationPasswordDoesntMatch()
+        {
+            //EntryInputText(string first, string last, string mobile, string email, string password, string confirm_password)
+            EntryInputText("firstname", "lastname", "0401420594", "unique95@gmail.com", "aaaaaa", "bbbbbb");
+            WaitAndTest(10000, "Password Doesn't match.");
+        }
+
+
+        //password length should be greater than 5
+        [Test]
+        public void RegistrationPasswordLength()
+        {
+            //EntryInputText(string first, string last, string mobile, string email, string password, string confirm_password)
+            EntryInputText("firstname", "lastname", "0401420594", "unique95@gmail.com", "abcde", "abcde");
+            WaitAndTest(10000, "Password Length should be greater than 5.");
+        }
+
+        //entering invalid mobile number format
+        [Test]
+        public void RegistrationMobile1()
+        {
+            //EntryInputText(string first, string last, string mobile, string email, string password, string confirm_password)
+            EntryInputText("firstname", "lastname", "12345", "unique95@gmail.com", "bbbbbb", "bbbbbb");
+            WaitAndTest(10000, "Mobile Number length must be 10 and consist of numerical numbers.");
+        }
+
+        //entering invalid mobile number format
+        [Test]
+        public void RegistrationMobile2()
+        {
+            //EntryInputText(string first, string last, string mobile, string email, string password, string confirm_password)
+            EntryInputText("firstname", "lastname", "eafqfqwqfqw", "unique95@gmail.com", "bbbbbb", "bbbbbb");
+            WaitAndTest(10000, "Mobile Number length must be 10 and consist of numerical numbers.");
+        }
+
+        //check first name field can be number or special characters
+        [Test]
+        public void RegistrationName1()
+        {
+            //EntryInputText(string first, string last, string mobile, string email, string password, string confirm_password)
+            EntryInputText("32424", "aafawfawf", "0401420594", "unique95@gmail.com", "bbbbbb", "bbbbbb");
+            WaitAndTest(10000, "Your first name or last name is not a name");
+        }
+
+        //check last name field can be number or special characters
+        [Test]
+        public void RegistrationName2()
+        {
+            //EntryInputText(string first, string last, string mobile, string email, string password, string confirm_password)
+            EntryInputText("aefafafaf", "%@#%@%", "0401420594", "unique95@gmail.com", "bbbbbb", "bbbbbb");
+            WaitAndTest(10000, "Your first name or last name is not a name");
+        }
+
+
+        //check email format
+        [Test]
+        public void RegistrationEmailFormat1()
+        {
+            //EntryInputText(string first, string last, string mobile, string email, string password, string confirm_password)
+            EntryInputText("firstname", "lastname", "0401420594", "unique95", "bbbbbb", "bbbbbb");
+            WaitAndTest(10000, "Please check the format of your email.");
+        }
+
+        //check whether registration can happen successfully
+        [Test]
+        public void RegistrationIsItSuccessful()
+        {
+            /*
+            //EntryInputText(string first, string last, string mobile, string email, string password, string confirm_password)
+            EntryInputText("firstname", "lastname", "0401420594", "unique95@gmail.com", "bbbbbb", "bbbbbb");
+            WaitAndTest(10000, "You have successfully registered.");
+            */
+        }
+
+
+
+
+
+
+
+
+        private void EntryInputText(string first, string last, string mobile, string email, string password, string confirm_password)
+        {
+            app.Tap("automationfirst");
+            app.EnterText(first);
+            app.Back();
+
+            app.Tap("automationlast");
+            app.EnterText(last);
+            app.Back();
+
+            app.Tap("automationmobile");
+            app.EnterText(mobile);
+            app.Back();
+
+            app.Tap("automationemail");
+            app.EnterText(email);
+            app.Back();
+
+
+            app.Tap("automationpassword");
+            app.EnterText(password);
+            app.Back();
+
+            app.ScrollDownTo("autoRegister", strategy: ScrollStrategy.Gesture, timeout: new TimeSpan(0, 1, 0));
+            app.Tap("passwordconfirm");
+            app.EnterText(confirm_password);
+            app.Back();
+
+
+            app.Tap("autoRegister");
+        }
+
+
+        //wait for the result because it takes a long time to fetch data from db
+        private void WaitAndTest(int stopTime, string alertDialogText)
+        {
+
+            stopwatch.Start();
+            bool continueLoop = true;
+            while (continueLoop)
+            {
+                if (stopwatch.ElapsedMilliseconds > stopTime)
+                {
+                    var expectedDialogAlert = app.Query(c => c.Class("AlertDialogLayout").Descendant().Text(alertDialogText)).FirstOrDefault().Text;
+                    Assert.IsTrue(expectedDialogAlert != null);
+                    stopwatch.Reset();
+                    stopwatch.Stop();
+                    continueLoop = false;
+                }
+
+            }
+        }
+
+
+
+
+
+    }
+}
---
+++ Cookout/Test/TestUserLogin.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using NUnit.Framework;
+using Xamarin.UITest;
+using Xamarin.UITest.Queries;
+
+namespace tryyyy.UITests
+{
+    [TestFixture(Platform.Android)]
+    public class TestUserLogin
+    {
+        IApp app;
+        Platform platform;
+        Stopwatch stopwatch;
+
+
+
+        public TestUserLogin(Platform platform)
+        {
+            this.platform = Platform.Android;
+        }
+
+        [SetUp]
+        public void BeforeEachTest()
+        {
+            app = AppInitializer.StartApp(platform);
+            stopwatch = new Stopwatch();
+
+
+        }
+     
+
+
+        //test whether any field is empty
+        [Test]
+        public void LogInEmptyTest1()
+        {
+            app.Screenshot("Log In");
+
+            app.Tap("automationEmail");
+            app.EnterText("");
+            app.Back();
+
+            app.Tap("automationPassword");
+            app.EnterText("");
+            app.Back();
+
+            app.Tap("automationLogin");
+
+            WaitAndTest(5000, "You can't leave any field empty.");
+
+
+        }
+        //test whether any field is empty
+        [Test]
+        public void LogInEmptyTest2()
+        {
+
+            app.Screenshot("Log In");
+
+            app.Tap("automationEmail");
+            app.EnterText("fawfawfawfaw");
+            app.Back();
+
+            app.Tap("automationPassword");
+            app.EnterText("");
+            app.Back();
+
+            app.Tap("automationLogin");
+
+            WaitAndTest(5000, "You can't leave any field empty.");
+
+
+        }
+        //test whether any field is empty
+        [Test]
+        public void LogInEmptyTest3()
+        {
+
+            app.Screenshot("Log In");
+
+            app.Tap("automationEmail");
+            app.EnterText("");
+            app.Back();
+
+            app.Tap("automationPassword");
+            app.EnterText("fawfawfawfaw");
+            app.Back();
+
+            app.Tap("automationLogin");
+
+            WaitAndTest(5000, "You can't leave any field empty.");
+
+
+        }
+        //test whether users cannot login with invalid user information in the database
+        [Test]
+        public void LogInWithInvalidUserTest()
+        {
+            //InvalidUserIdontexist@gmail.com - this email doesn't exist in db
+            app.Screenshot("Log In");
+
+            app.Tap("automationEmail");
+            app.EnterText("InvalidUserIdontexist@gmail.com");
+            app.Back();
+
+            app.Tap("automationPassword");
+            app.EnterText("fawfawfawfaw");
+            app.Back();
+
+            app.Tap("automationLogin");
+
+
+            WaitAndTest(10000, "The email doesn't exist");
+
+        }
+
+        //test whether the user can login successfully
+        [Test]
+        public void LogInWithValidUserTest()
+        {
+            //The following user already exists in the Database
+            //email: yyoon91@gmail.com
+            //password: aaaaaa (hashed password: ?\u0002E{\\A?d????\t????u(??^\u001a?[R?I?sW?)
+            app.Screenshot("Log In");
+
+            app.Tap("automationEmail");
+            app.EnterText("yyoon91@gmail.com");
+            app.Back();
+
+            app.Tap("automationPassword");
+            app.EnterText("aaaaaa");
+            app.Back();
+
+            app.Tap("automationLogin");
+
+            WaitAndTest(10000, "You now logged in.");
+        }
+
+
+
+        //test whether it would not allow when the user enters a wrong password
+        [Test]
+        public void LogInWithValidUserWrongPasswordTest()
+        {
+            //The following user already exists in the Database
+            //email: yyoon91@gmail.com
+            //password: aaaaaa (hashed password: ?\u0002E{\\A?d????\t????u(??^\u001a?[R?I?sW?)
+            app.Screenshot("Log In");
+
+            app.Tap("automationEmail");
+            app.EnterText("yyoon91@gmail.com");
+            app.Back();
+
+            app.Tap("automationPassword");
+            app.EnterText("aefawfwafqwfwafwaffwa");
+            app.Back();
+
+            app.Tap("automationLogin");
+
+            WaitAndTest(10000, "You have entered a wrong password.");
+        }
+        //wait for the result because it takes a long time to fetch data from db
+        private void WaitAndTest(int stopTime, string alertDialogText)
+        {
+
+            stopwatch.Start();
+            bool continueLoop = true;
+            while (continueLoop)
+            {
+                if (stopwatch.ElapsedMilliseconds > stopTime)
+                {
+                    var expectedDialogAlert = app.Query(c => c.Class("AlertDialogLayout").Descendant().Text(alertDialogText)).FirstOrDefault().Text;
+                    Assert.IsTrue(expectedDialogAlert != null);
+                    stopwatch.Reset();
+                    stopwatch.Stop();
+                    continueLoop = false;
+                }
+
+            }
+        }
+
+ 
+
+
+
+    }
+}
